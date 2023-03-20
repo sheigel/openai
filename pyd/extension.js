c25fa70bc2e25e49e686ee88d166eab63ba9f47d
Silviu Eigel <eigel.si@gmail.com>
2023-03-20 08:55:45 +0100

    Fix how we call openai by using official python package

--- pyd/extension.js
+++ pyd/extension.js
@@ -1,20 +1,72 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const OpenAI = require("openai-api");
+const {Configuration, OpenAIApi} = require("openai");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+async function describe_method(context, document, position, token){
+
+	console.log("Inside the hover")
+	const range = document.getWordRangeAtPosition(position);
+	const word = document.getText(range);
+		
+	const symbolsToFind = [vscode.SymbolKind.Function, vscode.SymbolKind.Method, vscode.SymbolKind.Constructor];
+
+	const docSymbols = await vscode.commands.executeCommand(
+		'vscode.executeDocumentSymbolProvider',
+		vscode.window.activeTextEditor.document.uri
+	) ;
+
+	const docSymbolsFunctionsMethods = docSymbols
+		? docSymbols.filter(symbol => symbolsToFind.includes(symbol.kind))
+		: undefined;
+
+	const docSymbolHovered = docSymbolsFunctionsMethods 
+		? docSymbolsFunctionsMethods.filter(symbol => symbol.range.contains(position))
+		: undefined;
+
+	if (docSymbolHovered != undefined) {
+		const functionBody = document.getText(docSymbolHovered[0].range)
+		let OPENAI_API_KEY = context.workspaceState.get("@codex.key1", "");
+
+		if (!OPENAI_API_KEY) {
+			await setApiKey();
+		}
+		const openai = new OpenAIApi(new Configuration({apiKey: OPENAI_API_KEY}));
+	
+		let editor = vscode.window.activeTextEditor;
+		let selection = editor.selection;
+		let activeCursor = editor.selection.active;
+		let text = editor.document.getText(selection);
+	
+		const prompt = `${functionBody}\n###\nDescribe above method:\n`
+		console.log("Sending prompt:", prompt)
+		const response = await openai.createCompletion({
+		  model: "code-davinci-002",
+		  prompt: prompt,
+		  temperature: 0,
+		  max_tokens: 150,
+		  top_p: 1.0,
+		  frequency_penalty: 0.0,
+		  presence_penalty: 0.0,
+		  stop: ["###"]
+		});
+
+		console.log(`Displaying result:\n ${response.data.choices[0].text}`)
+
+		return new vscode.Hover({
+			language: "Hello language",
+			value: "Silviu's bot thinks that: \n" + response.data.choices[0].text
+		});
+	}
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pyd" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -30,64 +82,7 @@ function activate(context) {
 
 	vscode.languages.registerHoverProvider('python', {
         async provideHover(document, position, token) {
-			console.log("Inside the hover")
-            const range = document.getWordRangeAtPosition(position);
-            const word = document.getText(range);
-				
-			const symbolsToFind = [vscode.SymbolKind.Function, vscode.SymbolKind.Method, vscode.SymbolKind.Constructor];
-
-			const docSymbols = await vscode.commands.executeCommand(
-				'vscode.executeDocumentSymbolProvider',
-				vscode.window.activeTextEditor.document.uri
-			) ;
-		
-			const docSymbolsFunctionsMethods = docSymbols
-				? docSymbols.filter(symbol => symbolsToFind.includes(symbol.kind))
-				: undefined;
-
-			const docSymbolHovered = docSymbolsFunctionsMethods 
-				? docSymbolsFunctionsMethods.filter(symbol => symbol.range.contains(position))
-				: undefined;
-
-            if (docSymbolHovered != undefined) {
-				functionBody = document.getText(docSymbolHovered[0].range)
-
-				if (!OPENAI_API_KEY) {
-					await setApiKey();
-				}
-				const openai = new OpenAI(OPENAI_API_KEY);
-			
-				let editor = vscode.window.activeTextEditor;
-				let selection = editor.selection;
-				let activeCursor = editor.selection.active;
-				let text = editor.document.getText(selection);
-			
-				prompt = `# Python 3 \n${functionBody} \n\n# Explanation of what the code does\n\n#`
-				const response = await openai.complete({
-				  engine: "code-davinci-002",
-				  prompt: prompt,
-				  temperature: 0,
-				  max_tokens: 150,
-				  top_p: 1.0,
-				  frequency_penalty: 0.0,
-				  presence_penalty: 0.0,
-				});
-
-				// const completion = await openai.complete({
-				// 	engine: "davinci",
-				// 	prompt: text,
-				// 	maxTokens: 100,
-				// });
-			
-				// editor.edit((editBuilder) => {
-				// 	editBuilder.insert(activeCursor, completion.data.choices[0].text);
-				// });
-
-                return new vscode.Hover({
-                    language: "Hello language",
-                    value: "Silviu's bot thinks that: \n" + response.data.choices[0].text
-                });
-            }
+			return await describe_method(context, document, position, token)
         }
     });
 
@@ -97,7 +92,7 @@ function activate(context) {
 		"OpenAI Codex Autocomplete has been Activated."
 	  );
 	
-	  let OPENAI_API_KEY = context.workspaceState.get("@codex.key", "");
+	  let OPENAI_API_KEY = context.workspaceState.get("@codex.key1", "");
 	
 	  async function setApiKey() {
 		let key = await vscode.window.showInputBox({
@@ -105,8 +100,8 @@ function activate(context) {
 		  title: "OpenAI API key",
 		  prompt: "Enter API Key here...",
 		});
-		context.workspaceState.update("@codex.key", key);
-		OPENAI_API_KEY = context.workspaceState.get("@codex.key", "");
+		context.workspaceState.update("@codex.key1", key);
+		OPENAI_API_KEY = context.workspaceState.get("@codex.key1", "");
 		return true;
 	  }
 	
@@ -125,30 +120,30 @@ function activate(context) {
 	  );
 	
 	  // AutoComplete Function
-	//   context.subscriptions.push(
-	// 	vscode.commands.registerCommand("openai-codex.autocomplete", async () => {
-	// 	  // Get API Key from Globalstate
-	// 	  if (!OPENAI_API_KEY) {
-	// 		await setApiKey();
-	// 	  }
-	// 	  const openai = new OpenAI(OPENAI_API_KEY);
+	  context.subscriptions.push(
+		vscode.commands.registerCommand("openai-codex.autocomplete", async () => {
+		  // Get API Key from Globalstate
+		  if (!OPENAI_API_KEY) {
+			await setApiKey();
+		  }
+		  const openai = new OpenAI(OPENAI_API_KEY);
 	
-	// 	  let editor = vscode.window.activeTextEditor;
-	// 	  let selection = editor.selection;
-	// 	  let activeCursor = editor.selection.active;
-	// 	  let text = editor.document.getText(selection);
+		  let editor = vscode.window.activeTextEditor;
+		  let selection = editor.selection;
+		  let activeCursor = editor.selection.active;
+		  let text = editor.document.getText(selection);
 	
-	// 	  const completion = await openai.complete({
-	// 		engine: "davinci",
-	// 		prompt: text,
-	// 		maxTokens: 100,
-	// 	  });
+		  const completion = await openai.complete({
+			engine: "davinci",
+			prompt: text,
+			maxTokens: 100,
+		  });
 	
-	// 	  editor.edit((editBuilder) => {
-	// 		editBuilder.insert(activeCursor, completion.data.choices[0].text);
-	// 	  });
-	// 	})
-	//   );
+		  editor.edit((editBuilder) => {
+			editBuilder.insert(activeCursor, completion.data.choices[0].text);
+		  });
+		})
+	  );
 }
 
 // This method is called when your extension is deactivated
